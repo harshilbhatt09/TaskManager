b069bd363775474945617f4d0b610cfe40106250
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-07-29 22:58:49 +0530

    Signup backend API

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./conn/conn")
+const cors = require("cors");
+const UserAPI = require("./routes/users")
+app.use(cors())
+app.use(express.json());
 
-app.use("/", (req, res)=>{
+app.use("/api/v1", UserAPI);
+app.use("/", (req, res) => {
     res.send("Hello from backend");
 })
 
 const PORT = 1000;
 
-app.listen(PORT, ()=>{console.log("server started")})
+app.listen(PORT, () => { console.log("server started") })
