c58b2df348a47ad59f7e483b32e7590801425766
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-07-30 17:16:43 +0530

    Task in database

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,14 @@ require("dotenv").config();
 require("./conn/conn")
 const cors = require("cors");
 const UserAPI = require("./routes/users")
+const TaskAPI = require("./routes/task")
+
 app.use(cors())
 app.use(express.json());
 
 app.use("/api/v1", UserAPI);
+app.use("/api/v2", TaskAPI);
+
 app.use("/", (req, res) => {
     res.send("Hello from backend");
 })
--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const Task = require("../models/task");
+const User = require("../models/users");
 
 router.post("/create-task", async (req, res) => {
     try {
         const { title, desc } = req.body;
-        const newTask = new Task(title, desc);
+        const { id } = req.headers;
+        const newTask = new Task({ title: title, desc: desc });
         const saveTask = await newTask.save();
+        const TaskID = saveTask._id;
+        await User.findByIdAndUpdate(id, { $push: { task: TaskID._id } })
+        res.status(200).json({ message: "Task Created" })
+
     } catch (error) {
         console.log(error)
         res.status(400).json({ message: "Internal Server Error" })
